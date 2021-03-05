8f80475a4c37f92aacbe7ba59178249ea5eaa45b
Piyush Daga <pdaga7775@conestogac.on.ca>
2021-03-05 02:30:52 -0500

    updated trianglesolver file

--- assignment2_tri/Program.cs
+++ assignment2_tri/Program.cs
@@ -10,6 +10,41 @@ namespace assignment2_tri
     {
         static void Main(string[] args)
         {
+            int num = 0;
+            int num1, num2, num3;
+            
+            while(num!=2)
+            {
+
+                Console.WriteLine("1. Enter Triangle Dimesions");
+                Console.WriteLine("2. Exit.");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                switch(num)
+                {
+                    case 1:
+
+                        Console.WriteLine("Enter 3 dimensions of triangle: ");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        num3 = Convert.ToInt32(Console.ReadLine());
+
+                        string str = TriangleSolver.Analyze(num1, num2, num3);
+
+                        Console.WriteLine("The numbers entered do form a traingle.");
+                        Console.WriteLine("It is an {0}",str);
+                        break;
+
+                    case 2:
+
+                        break;
+
+                }
+
+            }
+
         }
     }
+
+    
 }
--- assignment2_tri/TriangleSolver.cs
+++ assignment2_tri/TriangleSolver.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace assignment2_tri
 {
-    class TriangleSolver
+    public static class TriangleSolver
     {
+        
+        public static string Analyze(int x, int y, int z)
+        {
+            string triType;
+
+            if(x>0 && y>0 && z>0)
+            {
+                if ((x + y) > z && (y + z) > x && (z + x) > y)
+                {
+                    if ((x == y) && (y == z))
+                    {
+                        triType = "Equilateral Triangle";
+                    }
+                    else if ((x == y) || (y == z) || (z == x))
+                    {
+                        triType = "Isosceles Triangle";
+                    }
+                    else
+                    {
+                        triType = "Scalene Triangle";
+                    }
+                }
+                else
+                {
+                    triType = "Invalid input. Triangle cant be formed.";
+                }
+            }
+            else
+            {
+                triType = "Invalid input. Triangle cant be formed.";
+            }
+
+            return triType;
+        }
     }
 }
